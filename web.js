777fa4b2ee9f37c1f44ee39ae043bd9fe084d980
Hoagy Davis-Digges <Hoagy.davis.digges@gmail.com>
2013-07-21 22:12:15 +0000

    something

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs')
 app.use(express.logger());
 
+var file = fs.readFileSync('index.html')
+var output = file.toString()
+
 app.get('/', function(request, response) {
- res.format({'text/plain': function(){
- response.send('Hello World!')}
+ response.format({'text/plain': function(){
+ response.send(output)}
 })});
 
 var port = process.env.PORT || 5000;
